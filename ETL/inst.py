1c0bf0fc4ae20f7da3b4bea4d763ad4de67cb09d
Brian VanHoff <brian.vanhoff@gmail.com>
2020-04-22 18:57:04 -0400

    defined the instant class for the first time

--- ETL/inst.py
+++ ETL/inst.py
@@ -2,15 +2,16 @@
 
 class instant:
 
-    __self__ = {'instant': int,
-                'zipcode': str,
-                'forecasts': list,
-                'observations': dict
-                }
+    def __init__(instant, zipcode, forecasts, observation):
+        {'instant': instant,
+        'zipcode': zipcode,
+        'forecasts': forecasts,
+        'observation': observation
+        }
 
 
-def islegit(instant):
-    ''' check the instant's weathers array for count. if it is 40, then the document should be loaded to the remote database
+def itslegit(instant):
+    ''' check the instant's weathers array for count. if it is 40, then the document is returned
 
     :param instant: the instant docuemnt to be legitimized
     :type instant: dictionary
@@ -21,7 +22,7 @@ def islegit(instant):
     else:
         return False
 
-def getlegit(collection):
+def find_legit(collection):
     ''' find the 'legit' instants within the collection specified
 
     :param collection: database collection
@@ -29,16 +30,30 @@ def getlegit(collection):
     :return: list of documents
     '''
 
-    return [item if islegit(item) for item in collection.find({})]
+    return [item for item in collection.find({}) if islegit(item)]
 
-def loadlegit(collection):
+def load_legit(collection):
     ''' load the 'legit' instants to the remote database 
 
     :param collection: the collection you want to pull instants from
     :type collection: pymongo.collection.Collection
     '''
     from db_ops import Client, dbncol
-    from config import uri
+    from config import uri, host, port
+
+    legit_list = getlegit(collection)
+    client = Client(host, port)
+    col = dbncol(client, 'legit_inst', 'owmap')
+    col.insert_many(legit_list)
+
+def test_load_legit(collection):
+    ''' load the 'legit' instants to the remote database 
+
+    :param collection: the collection you want to pull instants from
+    :type collection: pymongo.collection.Collection
+    '''
+    from db_ops import Client, dbncol
+    from config import host, port
 
     legit_list = getlegit(collection)
     client = Client(uri)
