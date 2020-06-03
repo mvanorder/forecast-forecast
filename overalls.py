476a0451e1f2844ec05c1cd1b60492f5b398aa3d
Brian VanHoff <brian.vanhoff@gmail.com>
2020-06-03 19:33:09 -0400

    added all_values flatten flatten_d convert_flat

--- overalls.py
+++ overalls.py
@@ -38,12 +38,28 @@ def all_keys(d):
             keys.append(str(key))
     return keys
 
+def all_values(d, values=[]):
+    ''' Get all the dicitonary and nested dictionary keys from a dict, add them
+    to a list, return the list.
+    
+    :param d: A python dictionary
+    :return: A list of every key in the dictionary
+    '''
+
+    for key, value in d.items():
+        if isinstance(d[key], dict):
+            for sub_value in all_values(d[key], values=values):
+                continue
+        else:
+            values.append(value)
+    return values
+
 def compare_dicts(one, the_other):
-    ''' Compare the values of two dicts, key by common key. When the values are numbers
-    return the difference, when strings return 0 if the strings are equal and 1
-    if they are different, when dicts run this function, when NoneType set the
-    value to None.
-    *** When there is a key present in one and not the_other 
+    ''' Compare the values of two dicts, key by common key. When the values are
+    numbers return the difference, when strings return 0 if the strings are
+    equal and 1 if they are different, when dicts run this function, when
+    NoneType set the value to None.
+
     :params one, the_other: dictionaries with the same set of keys and sub-keys
     :type one, the_other: dict
     '''
@@ -69,3 +85,41 @@ def compare_dicts(one, the_other):
             print()
             print(f'missing key..... {e}')
     return delta
+
+def flatten(d):
+### I'm not 100% sure the order of the dict keys and values is preserved? ###
+    ''' Flatten a dict by zipping together a list of keys and nested keys and
+    a list of each key value.
+    
+    :param d: a dictionary
+    '''
+    
+    keys = all_keys(d)
+    values = all_values(d)
+    return dict(zip(keys, values))
+
+
+### Thanks to geeksforgeeks.org for the function. ###
+def flatten_dict(dd, separator ='.', prefix =''):
+    ''' Code to convert dict to flattened dictionary. '''
+    
+	return { prefix + separator + k if prefix else k : v
+			for kk, vv in dd.items()
+			for k, v in flatten_dict(vv, separator, kk).items()
+			} if isinstance(dd, dict) else { prefix : dd }
+
+
+### Thanks to geeksforgeeks.org for the function. ###
+from collections.abc import MutableMapping
+def convert_flatten(d, parent_key ='', sep ='.'):
+    ''' Code to convert a dict to a flattened dictionary. '''
+    
+	items = []
+	for k, v in d.items():
+		new_key = parent_key + sep + k if parent_key else k
+
+		if isinstance(v, MutableMapping):
+			items.extend(convert_flatten(v, new_key, sep = sep).items())
+		else:
+			items.append((new_key, v))
+	return dict(items)
